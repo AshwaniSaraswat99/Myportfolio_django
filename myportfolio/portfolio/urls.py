326c33c3f34511c50214756df2644db4e95f3561
Ashwani Saraswat <146135006+AshwaniSaraswat99@users.noreply.github.com>
2025-10-01 07:40:19 +0000

    made template pages for my portfolio

--- myportfolio/portfolio/urls.py
+++ myportfolio/portfolio/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns=[
     path("",views.home,name="home"),
-
+    path('projects',views.projects,name="projects"),
+    path('resume',views.resume,name="resume"),
+    path('contact',views.contact,name="contact"),
+    path('login',views.login,name="login"),
+    path('register',views.register,name="register"),
 ]
--- myportfolio/portfolio/views.py
+++ myportfolio/portfolio/views.py
@@ -4,4 +4,17 @@ from django.contrib import messages
 # Create your views here.
 def home(request):
     return render(request,"home.html")
-    
+def projects(request):
+    return render(request, "project.html")
+
+def resume(request):
+    return render(request, "resume.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def register(request):
+    return render(request, "register.html")
